8507be8b9f8f5330d9b1508de229c6cfa462935f
Holly Teunis <+>
2021-10-26 08:56:39 -0400

    restyled with placeholder image

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-// ii
+const display = document.querySelector("#Jacket")
+const placeHolder = document.createElement('img')
+placeHolder.src = 'Question-Mark.png'
+display.append(placeHolder)
 
-//small change 
+ liker = document.querySelector("#liker")
+
+liker.addEventListener('click', () => {
+    return console.log('test')
+}
+)
